4a82e8eaf80d708f7a5ee68bf4fca922c61f9c40
Federico Rocchetti <federico_rocchetti@hotmail.com>
2022-12-20 13:49:46 -0500

    Parts 1 through 3 complete.

---
+++ crud.py
@@ -0,0 +1,52 @@
+"""CRUD operations"""
+
+from model import db, User, Movies, Ratings, connect_to_db
+
+def create_user(email, password):
+    """Create and return a new user"""
+
+    user = User(email=email, password=password)
+    return user
+
+def get_users():
+    """Returns all users"""
+
+    return User.query.all()
+
+def get_user_by_id(user_id):
+    """Returns user by id."""
+
+    return User.query.get(user_id)
+
+def create_movie(title, overview, release_date, poster_path):
+    """Create and return a new movie"""
+
+    movie = Movies(title=title,
+        overview=overview,
+        release_date=release_date,
+        poster_path=poster_path
+    )
+
+    return movie
+
+def get_movies():
+    """Return all movies."""
+
+    return Movies.query.all()
+
+def get_movie_by_id(movie_id):
+    """Returns movie by id."""
+
+    return Movies.query.get(movie_id)
+
+def create_rating(user, movie, score):
+    """Create and return a rating"""
+
+    rating = Ratings(user=user, movie=movie, score=score)
+
+    return rating
+
+
+if __name__ == '__main__':
+    from server import app
+    connect_to_db(app)
